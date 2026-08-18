c41ac68dbb800f9e616ac48e423fd30003a3efec
Damstor <3194540@gmail.com>
2026-08-18 16:03:05 -0500

    Rename VentaRepository and remove method
    Renamed VentaRepository to VentanaRepository and removed method.

--- src/main/java/com/example/demo/repository/VentaRepository.java
+++ src/main/java/com/example/demo/repository/VentaRepository.java
@@ -3,8 +3,5 @@ package com.example.demo.repository;
 import com.example.demo.model.Venta;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
-public interface VentaRepository extends JpaRepository<Venta, Long> {
-    List<Venta> findAllByOrderByFechaDesc();
+public interface VentanaRepository extends Jparepository<Venta,Long>{
 }
